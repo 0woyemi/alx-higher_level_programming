2e02317d9dc7127441e3799ef3d271ed0175aeb9
0woyemi <owoeyeowoyemi@gmail.com>
2024-01-12 16:13:38 -0800

    Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    if a_dictionary is not None:
-        return(len(a_dictionary.keys()))
-    return(0)
+    num = 0
+    list_keys = list(a_dictionary.keys())
+
+    for i in list_keys:
+        num += 1
+
+    return (num)
